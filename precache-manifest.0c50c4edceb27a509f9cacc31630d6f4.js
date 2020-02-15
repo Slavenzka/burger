f2660e0831a8f9f24e206cd98cc0713714cb4c20
Ivan <i.romanov@protonmail.com>
2020-02-15 13:26:26 +0300

    Updates

---
+++ precache-manifest.0c50c4edceb27a509f9cacc31630d6f4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb87a558bd6473fb416c80517862d4ff",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "6313ab46a3346b612897",
+    "url": "/burger/static/css/3.4a7326c8.chunk.css"
+  },
+  {
+    "revision": "cb371768bb5b6bc7ab07",
+    "url": "/burger/static/css/4.bffcc0ef.chunk.css"
+  },
+  {
+    "revision": "22adadd917ed29c30d1d",
+    "url": "/burger/static/css/5.47485be8.chunk.css"
+  },
+  {
+    "revision": "8182775bd135e002e663",
+    "url": "/burger/static/css/main.1f21fb62.chunk.css"
+  },
+  {
+    "revision": "4415006e1a217d6b7ec2",
+    "url": "/burger/static/js/2.b9c5ddde.chunk.js"
+  },
+  {
+    "revision": "6313ab46a3346b612897",
+    "url": "/burger/static/js/3.aaddb036.chunk.js"
+  },
+  {
+    "revision": "cb371768bb5b6bc7ab07",
+    "url": "/burger/static/js/4.011802f7.chunk.js"
+  },
+  {
+    "revision": "22adadd917ed29c30d1d",
+    "url": "/burger/static/js/5.45eca391.chunk.js"
+  },
+  {
+    "revision": "8182775bd135e002e663",
+    "url": "/burger/static/js/main.01cedbe9.chunk.js"
+  },
+  {
+    "revision": "ec976a44a07c5b7b91f2",
+    "url": "/burger/static/js/runtime~main.fe7f647b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "48aa7ad98beedc63d82925f45fd8e72c",
+    "url": "/burger/static/media/opensans.48aa7ad9.woff2"
+  },
+  {
+    "revision": "811c8baeca3bb3329488f729979c1fbc",
+    "url": "/burger/static/media/opensans.811c8bae.woff"
+  },
+  {
+    "revision": "313824e85557a721d326424b6eb63fec",
+    "url": "/burger/static/media/opensansbold.313824e8.woff2"
+  },
+  {
+    "revision": "6f05808b4f6a11d3feb1716291c7c038",
+    "url": "/burger/static/media/opensansbold.6f05808b.woff"
+  }
+]);
